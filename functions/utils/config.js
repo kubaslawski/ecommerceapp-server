0cf737e6f1e3d435347f61018d248facb8a2c423
kubaslawski <kubaslawski.it@gmail.com>
2022-07-14 09:43:56 +0200

    1. Config files

---
+++ functions/utils/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyCU2gr-0A0LBRL1O_3IZIRbPvlFrncwVfs",
+    authDomain: "ecommerceapp-e7305.firebaseapp.com",
+    projectId: "ecommerceapp-e7305",
+    storageBucket: "ecommerceapp-e7305.appspot.com",
+    messagingSenderId: "830382715281",
+    appId: "1:830382715281:web:a52886255d7031ed15aa30",
+    measurementId: "G-V7S0S13C5F"
+}
